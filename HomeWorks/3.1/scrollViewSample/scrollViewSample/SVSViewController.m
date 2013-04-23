9ec5ac2a9efddec8850fbc44a9ab186e144403da
長田 乾 <ken.osada@90201709n.intra.emercury.co.jp>
2013-04-23 19:11:19 +0900

    kadai 3

--- HomeWorks/3.1/scrollViewSample/scrollViewSample/SVSViewController.m
+++ HomeWorks/3.1/scrollViewSample/scrollViewSample/SVSViewController.m
@@ -9,7 +9,7 @@
 #import "SVSViewController.h"
 
 @interface SVSViewController ()
-
+@property(nonatomic,strong) UIScrollView *scroll;
 @end
 
 @implementation SVSViewController
@@ -18,6 +18,26 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    _scroll = [[UIScrollView alloc]initWithFrame:self.view.frame];
+    [self.view addSubview:_scroll];
+    
+    UIImage *image = [UIImage imageNamed:@"big_image.jpg"];
+    UIImageView *imageView = [[UIImageView alloc]initWithFrame:CGRectMake(0.0f, 0.0f, image.size.width, image.size.height)];
+    
+    imageView.image = image;
+    [_scroll addSubview:imageView];
+    _scroll.contentSize = imageView.frame.size;
+    
+    _scroll.maximumZoomScale = 3.0f;
+    _scroll.minimumZoomScale = 0.5f;
+    
+    _scroll.delegate = self;
+    
+    [UIView animateWithDuration:4.0
+                     animations:^{
+                         [_scroll setContentOffset:CGPointMake(40.0f, 40.0f)];
+                         [_scroll setZoomScale:2.0f];
+                     }];
 
 }
 
@@ -27,5 +47,17 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView{
+    for (UIView *view in scrollView.subviews ){
+        if( [view isKindOfClass:[UIImageView class]]){
+            return view;
+        }
+    }
+    return nil;
+}
 
+-(void) scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate{
+    
+    NSLog(@"%f , %f" , scrollView.contentOffset.x , scrollView.contentOffset.y);
+}
 @end
